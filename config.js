aaebd4a7f0fe8f3204463996277bb463e095f440
ngthudctun <155500535+ngthudctun@users.noreply.github.com>
2025-10-16 17:09:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    lines: [
+        "Chúc mừng mọi người ngày 20-10",
+        "Chúc các bạn luôn xinh đẹp, vui vẻ",
+        "Chúc các bạn luôn thành công trong cuộc sống",
+        "Chúc các bạn luôn hạnh phúc bên gia đình và người thân",
+        "Chúc các bạn có một ngày 20-10 thật ý nghĩa",
+        "Tặng các bạn món quà nhỏ này, hi vọng các bạn thích",
+        "Yêu mọi người nhiều lắm <3 <3 <3",
+        
+
+        
+    ],
+}
